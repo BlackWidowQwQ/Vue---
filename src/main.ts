3b9c4c999e602903f1908c4ffd4fd68eef1363b0
BlackWidowQwQ <489378072@qq.com>
2020-09-04 16:01:56 +0800

    添加二维码

--- src/main.ts
+++ src/main.ts
@@ -29,4 +29,12 @@ window.onload = function() {
 
 if (document.documentElement.clientWidth > 500) {
   window.alert("请使用手机打开页面，效果更佳");
+  const img = document.createElement("img");
+  img.src = "/QRcode.png";
+  img.style.position = "fixed";
+  img.style.left = "50%";
+  img.style.top = "50%";
+  img.style.transform = "translate(-50%,-50%)";
+  img.style.boxShadow = "0 0 10px rgba(0,0,0,0.25)";
+  document.body.appendChild(img);
 }
